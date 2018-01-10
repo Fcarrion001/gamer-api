aeafbc9f90b7eb5bde222938cd2742b95666b4a0
fcarrion001 <fpc1118@gmail.com>
2018-01-10 16:16:36 -0500

    Got rid of some console prints
    Removed some prints in the Games controller.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,10 +6,6 @@ class GamesController < OpenReadController
   def indexapi
     # user_key given by 3rd party api
     user_key = ENV['USER_KEY']
-    p user_key
-    p "route is being hit"
-    p 'this is params', params[:id]
-    p 'this is :params', :params
     # headers and get request required by the api in the format that the gem requires
     @games = HTTP.headers(:accept => 'application/json', 'user-key' => user_key)
               .get("https://api-2445582011268.apicast.io/games/?fields=*&filter[release_dates.platform][eq]=48&filter[popularity][gte]=8&filter[first_release_date][gte]=1504144194000&limit=50&scroll=1").to_s
@@ -20,8 +16,6 @@ class GamesController < OpenReadController
 
   def showapi
     user_key = ENV['USER_KEY']
-    p 'this is params', params[:id]
-      p 'this is :params', :params
     @game = HTTP.headers(:accept => 'application/json', 'user-key' => user_key)
               .get("https://api-2445582011268.apicast.io/games/#{params[:id]}?fields=name,first_release_date,storyline,summary,total_rating,genres,popularity&expand=genres&filter[summary][exists]=true").to_s
 
